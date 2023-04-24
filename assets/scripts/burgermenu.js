b27a847d63dbac1bd3a90515c66ae98027197b28
NicolineMertz <Nicolinekm@gmail.com>
2023-04-24 14:14:43 +0200

    add a whole bunch of stuff

---
+++ assets/scripts/burgermenu.js
@@ -0,0 +1,16 @@
+(function(){
+    const BUTTON = document.querySelector(".burgerMenuButton")
+    const MENU = document.querySelector(".primaryNavigation")
+
+    BUTTON.addEventListener("click", clickHandler)
+
+    function clickHandler() {
+        MENU.classList.toggle("primaryNavigation--show")
+
+        if (MENU.ariaHidden == "false") {
+            MENU.setAttribute("aria-hidden", true)
+        } else {
+            MENU.setAttribute("aria-hidden", false)
+        }
+    }
+})()
